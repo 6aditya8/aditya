3022fb57f03011f4b9df0f257b27d6720ddba886
adityag <6aditya8@gmail.com>
2018-07-24 17:47:27 +0530

    add backend

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.generics import RetrieveUpdateAPIView
 
 from .serializers import RegistrationSerializer, LoginSerializer, UserSerializer
 from .renderers import UserJSONRenderer
