779e0b643b1eea3c0de4598efcaf9217848a12d0
adityag <6aditya8@gmail.com>
2018-07-24 16:07:58 +0530

    login

--- jwt_auth/settings.py
+++ jwt_auth/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication'
+    'rest_framework',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATIC_URL = '/static/'
 
--- jwt_auth/urls.py
+++ jwt_auth/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-     url(r'^api/', include('jwt_auth.authentication.urls', namespace='authentication')),
+     url(r'^api/', include('authentication.urls', namespace='authentication')),
 ]
